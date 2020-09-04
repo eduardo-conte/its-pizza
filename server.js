39c4a6355fa06bf54300b2336649c0e4182859f2
eduardo-conte <duduconte@gmail.com>
2020-09-04 19:46:15 -0300

    primeiro index

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());// parse requests of content-type - application/json
 app.use(bodyParser.urlencoded({ extended: true }));// parse requests of content-type - application/x-www-form-urlencoded
 
 app.use(express.static("./public"));
-app.use(favicon('./public/img/favicon.png'));
+app.use(favicon('./public/images/favicon.png'));
 
 //Database
 // const db = require("./app/models");
